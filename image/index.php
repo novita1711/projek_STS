8a0ebfcaec46e8506d6823afbb4f423857a0b0a7
Maulanazahronyyy <maulanazahronyyy@gmail.com>
2023-03-28 12:29:16 +0700

    first commit

---
+++ image/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="nav">
+            <div class="logo">
+                <a href="#">
+                    <p>Burger Big</p>
+                </a>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Food</li>
+                <li>Hotel</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div class="header">
+            <h1>Burger<span>Big</span></h1>
+        </div>
+
+        <div class="content-header">
+            <p>Hamburger atau burger adalah sejenis roti berbentuk bundar yang diiris dua, dan di tengahnya diisi dengan patty yang biasanya diambil dari daging, kemudian sayur-sayuran berupa selada, tomat dan bawang bombai.</p>
+            <br>
+            <input type="text" name="text" class="search-input" placeholder="cari tempat kuliner...">
+            <input class="search-btn" type="button" value="Cari">
+        </div>
+         
+        <div class="col-img">
+            <img src="image/burger.jpg" alt="">
+        </div>
+
+    </div>
+</body>
+</html>
